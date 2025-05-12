6f64ddd09c1619869edf660d8a360d26124e8daf
Juhasen <juszczyk-krystian@wp.pl>
2025-05-13 01:27:50 +0200

    Added methods to generate tokens with or without extra claims

--- backend/src/main/java/pl/juhas/backend/config/JwtService.java
+++ backend/src/main/java/pl/juhas/backend/config/JwtService.java
@@ -2,11 +2,17 @@ package pl.juhas.backend.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
 @Service
@@ -14,16 +20,31 @@ public class JwtService {
 
     private static final String SECRET_KEY = "MIpnm5ACc+l4XCPjSZCHcRaWR2pP1Kh/WDNjFmK5Ckhtn9PikhIKeJ3Kr9t0xIPheicND7nyhBonaRQpJ4PYHK2zeF3kkhj5Aqk9IRW0aWYW60tFwu1WMlxWN0W1d8ANb+t7a2ToYyycY1138Osna5U+CMnPrYnm/n9YKjTCe6EIXNznUO0JqK/8aqpEWXCHclW9JDWvyvgKEpv9E2ofYrnZ/UIuV2exh9l8cnKtQ46w+leRnaADgE24b1myIa0hdihDcbDktGAnGnVzwGdvmxSE1hTW3JnCRinczllOe+Vt+2QUS1QxBmVJP9xPOfsI6+v0CHtC5pmTiW28hSSns4iqCHNteH9rGKGX0AthLyQ=";
 
-    public String extractUsername(String token){
+    public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
     }
 
-    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver){
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
         final Claims claims = extractAllClaims(token);
         return claimsResolver.apply(claims);
     }
 
-    private Claims extractAllClaims(String token){
+    public String generateToken(UserDetails userDetails) {
+        return generateToken(new HashMap<>(), userDetails);
+    }
+
+    public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
+        return Jwts
+                .builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24)) // Valid for 24hours + 1000 millis
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    private Claims extractAllClaims(String token) {
         return Jwts
                 .parserBuilder()
                 .setSigningKey(getSignInKey())
