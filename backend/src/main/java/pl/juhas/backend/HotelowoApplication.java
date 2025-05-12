dd5a4a72db7d5aaf4a43cbe8108dcea085022d07
Juhasen <juszczyk-krystian@wp.pl>
2025-05-12 23:49:28 +0200

    Database connection config

---
+++ backend/src/main/java/pl/juhas/backend/HotelowoApplication.java
@@ -0,0 +1,13 @@
+package pl.juhas.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelowoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelowoApplication.class, args);
+    }
+
+}
