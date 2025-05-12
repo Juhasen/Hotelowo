c612d4978150ab03858ef2c7df8679f8309ee253
Juhasen <juszczyk-krystian@wp.pl>
2025-05-13 00:36:25 +0200

    Creating user class

---
+++ backend/src/main/java/pl/juhas/backend/user/User.java
@@ -0,0 +1,25 @@
+﻿package pl.juhas.backend.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class User {
+
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String password;
+
+}
