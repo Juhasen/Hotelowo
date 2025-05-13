0c857bdca883777916aa8519b22f4de1f275fb27
Juhasen <juszczyk-krystian@wp.pl>
2025-05-13 09:28:47 +0200

    Implemented register method

--- backend/src/main/java/pl/juhas/backend/auth/AuthenticationService.java
+++ backend/src/main/java/pl/juhas/backend/auth/AuthenticationService.java
@@ -1,14 +1,37 @@
 package pl.juhas.backend.auth;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import pl.juhas.backend.config.JwtService;
+import pl.juhas.backend.user.Role;
+import pl.juhas.backend.user.User;
+import pl.juhas.backend.user.UserRepository;
 
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService {
 
+    private final UserRepository userRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    private final JwtService jwtService;
+
     public AuthenticationResponse register(RegisterRequest request) {
-        return null;
+        var user = User.builder()
+                .firstName(request.getFirstname())
+                .lastName(request.getLastname())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+
+        userRepository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
     }
 
     public AuthenticationRequest authenticate(AuthenticationRequest request) {
