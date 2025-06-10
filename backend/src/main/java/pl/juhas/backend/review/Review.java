3e986bdfe52712b91c1a2ddaf750f0722c879b80
Juhasen <juszczyk-krystian@wp.pl>
2025-06-11 00:38:31 +0200

    Add Review entity with relationships to User and Hotel, and include rating, comment, and createdAt fields

--- backend/src/main/java/pl/juhas/backend/review/Review.java
+++ backend/src/main/java/pl/juhas/backend/review/Review.java
@@ -5,6 +5,11 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.With;
+import org.springframework.data.annotation.CreatedDate;
+import pl.juhas.backend.hotel.Hotel;
+import pl.juhas.backend.user.User;
+
+import java.time.LocalDateTime;
 
 @Data
 @With
@@ -16,5 +21,22 @@ public class Review {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "hotel_id", nullable = false)
+    private Hotel hotel;
+
+    private Byte rating;
+
+    private String comment;
+
+    @CreatedDate
+    @Column(name ="created_at", updatable = false)
+    private LocalDateTime createdAt;
+
 }
 
