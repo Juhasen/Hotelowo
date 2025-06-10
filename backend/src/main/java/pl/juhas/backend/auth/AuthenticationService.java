f98990af677085889062769dd5442ca16f966160
Juhasen <juszczyk-krystian@wp.pl>
2025-06-10 13:59:46 +0200

    Add createdAt field to user in AuthenticationService

--- backend/src/main/java/pl/juhas/backend/auth/AuthenticationService.java
+++ backend/src/main/java/pl/juhas/backend/auth/AuthenticationService.java
@@ -17,6 +17,7 @@ import pl.juhas.backend.user.User;
 import pl.juhas.backend.user.UserRepository;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
@@ -35,6 +36,7 @@ public class AuthenticationService {
                 .email(request.getEmail())
                 .phoneNumber(request.getPhoneNumber())
                 .password(passwordEncoder.encode(request.getPassword()))
+                .createdAt(LocalDateTime.now())
                 .role(request.getRole())
                 .build();
         var savedUser = repository.save(user);
