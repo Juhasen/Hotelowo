cac9d767666a5ff35ce20f9c657a7c4cf0de6433
Juhasen <juszczyk-krystian@wp.pl>
2025-06-11 00:38:21 +0200

    Add Reservation entity with relationships to User and Room, and include status and createdAt fields

--- backend/src/main/java/pl/juhas/backend/reservation/Reservation.java
+++ backend/src/main/java/pl/juhas/backend/reservation/Reservation.java
@@ -5,6 +5,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.With;
+import org.springframework.data.annotation.CreatedDate;
+import pl.juhas.backend.room.Room;
+import pl.juhas.backend.user.User;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @Data
 @With
@@ -16,5 +22,29 @@ public class Reservation {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "room_id", nullable = false)
+    private Room room;
+
+    @Column(nullable = false)
+    private LocalDateTime checkIn;
+
+    @Column(nullable = false)
+    private LocalDateTime checkOut;
+
+    @Enumerated(value = EnumType.STRING)
+    private Status status;
+
+    @Column(precision = 10, scale = 2)
+    private BigDecimal totalPrice;
+
+    @CreatedDate
+    @Column(name ="created_at", updatable = false)
+    private LocalDateTime createdAt;
 }
 
