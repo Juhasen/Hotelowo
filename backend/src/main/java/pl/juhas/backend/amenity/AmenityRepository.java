a094972c2eca880508a1382cb39191c3a3d97a75
Juhasen <juszczyk-krystian@wp.pl>
2025-06-11 01:39:59 +0200

    Add EAGER fetch type for hotels in Amenity entity and implement custom query in AmenityRepository

--- backend/src/main/java/pl/juhas/backend/amenity/AmenityRepository.java
+++ backend/src/main/java/pl/juhas/backend/amenity/AmenityRepository.java
@@ -1,7 +1,12 @@
 package pl.juhas.backend.amenity;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface AmenityRepository extends JpaRepository<Amenity, Integer> {
-
+    @Query("SELECT a FROM Amenity a WHERE a.id IN :ids")
+    List<Amenity> findAmenitiesByIds(Integer[] ids);
 }
