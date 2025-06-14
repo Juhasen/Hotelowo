3bb1d1f9366c724fe439424234e80d7eec9e9e30
Juhasen <juszczyk-krystian@wp.pl>
2025-06-14 13:26:11 +0200

    Add primary and secondary brown colors to Tailwind CSS configuration

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+const {primaryBrown, secondaryBrown} = require("./src/app/[locale]/lib/themeColors");
 module.exports = {
   content: ["./src/**/*.{html,js,jsx,tsx,ts}"],
   theme: {
+    colors:[
+        primaryBrown,
+        secondaryBrown
+    ]
 
   },
   plugins: [],
