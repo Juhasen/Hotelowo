c1d35973bc46d146df93b7af2a80bd7a408ed1ce
Juhasen <juszczyk-krystian@wp.pl>
2025-06-25 20:12:04 +0200

    Add hotel reservation API route with error handling

---
+++ frontend/src/app/api/reservation/[id]/route.ts
@@ -0,0 +1,36 @@
+﻿import { NextResponse } from 'next/server';
+import {BASE_API_URL} from "@/app/[locale]/lib/utils";
+
+export async function GET(request: Request) {
+    try {
+        // Pobieranie parametrów z URLa
+        const url = new URL(request.url);
+        const id = url.pathname.split('/').pop(); // Pobieranie ID z końca ścieżki
+        if (!id) {
+            console.error('Hotel ID is missing in the request URL');
+            return NextResponse.json({ error: 'Hotel ID is required' }, { status: 400 });
+        }
+
+        const response = await fetch(`${BASE_API_URL}/reservation/${id}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            cache: 'no-store', // Wyłączenie cache'owania
+        });
+
+        if (!response.ok) {
+            console.error(`Backend returned status: ${response.status}`);
+            throw new Error(`Backend API error: ${response.statusText}`);
+        }
+
+        // Przetwarzanie odpowiedzi
+        const data = await response.json();
+
+        // Zwracamy dane w formatowaniu zgodnym z oczekiwaniami frontendu
+        return NextResponse.json(data);
+    } catch (error) {
+        console.error('Error in hotel search API:', error);
+        return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+    }
+}
