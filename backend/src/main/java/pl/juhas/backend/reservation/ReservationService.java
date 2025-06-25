639015db111eb7b31b93a93ee39bb6d21a0b6ada
Juhasen <juszczyk-krystian@wp.pl>
2025-06-25 04:39:06 +0200

    Add confirmReservation method to handle reservation confirmation logic

--- backend/src/main/java/pl/juhas/backend/reservation/ReservationService.java
+++ backend/src/main/java/pl/juhas/backend/reservation/ReservationService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import pl.juhas.backend.hotel.LocaleType;
 import pl.juhas.backend.reservation.dto.ReservationPreviewRequest;
 import pl.juhas.backend.reservation.dto.ReservationPreviewResponse;
+import pl.juhas.backend.reservation.dto.ReservationRequest;
 import pl.juhas.backend.user.User;
 import pl.juhas.backend.utils.DateParser;
 
@@ -43,5 +44,31 @@ public class ReservationService {
 
 
         return ReservationMapper.toPreviewResponse(request, locale, user, startDate, endDate);
+    }
+
+    public ReservationPreviewResponse confirmReservation(ReservationRequest request, Principal connectedUser, LocaleType locale) {
+        List<LocalDate> checkInOutDates = DateParser.parseCheckDates(request.checkInDate(), request.checkOutDate());
+        if (checkInOutDates.isEmpty()) {
+            System.out.println("Invalid check-in or check-out date format");
+            return null;
+        }
+
+        LocalDate startDate = checkInOutDates.get(0);
+        LocalDate endDate = checkInOutDates.get(1);
+
+        if (startDate.isAfter(endDate)) {
+            System.out.println("Check-in date cannot be after check-out date");
+            return null;
+        }
+        if (startDate.isBefore(LocalDate.now())) {
+            System.out.println("Check-in date cannot be in the past");
+            return null;
+        }
+
+        var user = (User) ((UsernamePasswordAuthenticationToken) connectedUser).getPrincipal();
+
+        Reservation reservation = reservationRepository.save(ReservationMapper.toReservation(request, user, startDate, endDate));
+
+        return ReservationMapper.toResponse(reservation, locale, user);
     }
 }
