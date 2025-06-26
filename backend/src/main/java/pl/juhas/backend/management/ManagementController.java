35c7b8c284f4e910437053f40658279ea0efed44
Juhasen <juszczyk-krystian@wp.pl>
2025-06-26 02:02:41 +0200

    Add management service and endpoint for retrieving amenities by locale

--- backend/src/main/java/pl/juhas/backend/management/ManagementController.java
+++ backend/src/main/java/pl/juhas/backend/management/ManagementController.java
@@ -1,11 +1,26 @@
 package pl.juhas.backend.management;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import pl.juhas.backend.amenity.dto.AmenityResponse;
+import pl.juhas.backend.hotel.LocaleType;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/management")
 public class ManagementController {
+    private final ManagementService managementService;
+
+    public ManagementController(ManagementService managementService) {
+        this.managementService = managementService;
+    }
+
+    @GetMapping("/amenities/{locale}")
+    public ResponseEntity<List<AmenityResponse>> getAmenities(@PathVariable LocaleType locale) {
+        return ResponseEntity.ok(managementService.getAmenities(locale.name()));
+    }
 
     @PostMapping("/create/hotel")
     public String createHotel() {
---
+++ backend/src/main/java/pl/juhas/backend/management/ManagementService.java
@@ -0,0 +1,28 @@
+package pl.juhas.backend.management;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import pl.juhas.backend.amenity.AmenityRepository;
+import pl.juhas.backend.amenity.dto.AmenityResponse;
+import pl.juhas.backend.hotel.LocaleType;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor
+public class ManagementService {
+    private final AmenityRepository amenityRepository;
+
+    public List<AmenityResponse> getAmenities(String locale) {
+        return amenityRepository.findAll().stream()
+                .map(amenity -> {
+                    String name = locale.equals(LocaleType.pl.name())
+                            ? amenity.getName_pl()
+                            : amenity.getName_en();
+                    return new AmenityResponse(name, amenity.getIcon());
+                })
+                .collect(Collectors.toList());
+    }
+
+}
