dbc516805abd07ff935a1fff9e9ccb72dbb65ffa
Juhasen <juszczyk-krystian@wp.pl>
2025-06-26 02:02:19 +0200

    Add management route for amenities with authorization and error handling

---
+++ frontend/src/app/api/admin/amenities/route.ts
@@ -0,0 +1,42 @@
+﻿import {NextResponse} from 'next/server';
+import {BASE_API_URL} from "@/app/[locale]/lib/utils";
+import {getTranslations} from "next-intl/server";
+import {getSession} from '@/app/[locale]/lib/session';
+import {getLocale} from "next-intl/server";
+export async function GET() {
+    try {
+        const t = await getTranslations({locale: 'pl', namespace: ''});
+
+        const user = await getSession();
+
+        if (!user || !user.userToken) {
+            return NextResponse.json({error: t('API.unauthorized')}, {status: 401});
+        }
+
+        const locale = await getLocale();
+
+        const response = await fetch(`${BASE_API_URL}/management/amenities/${locale}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${user.userToken}`
+            },
+            cache: 'no-store', // Wyłączenie cache'owania
+        });
+
+        if (!response.ok) {
+            if (response.status === 409) {
+                return NextResponse.json({error: t('API.reviewAlreadyExists')}, {status: 409});
+            }
+            console.error(`Backend returned status: ${response.status}`);
+            throw new Error(`Backend API error: ${response.statusText}`);
+        }
+
+        const data = await response.json();
+        // Zwracamy dane w formatowaniu zgodnym z oczekiwaniami frontendu
+        return NextResponse.json(data);
+    } catch (error) {
+        console.error('Error in hotel search API:', error);
+        return NextResponse.json({error: 'Internal Server Error'}, {status: 500});
+    }
+}
